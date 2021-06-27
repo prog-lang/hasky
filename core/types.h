992095189704c0ec611447fe7ea66514b23cee45
Viktor A. Rozenko Voitenko <sharp.vik@gmail.com>
2021-06-27 12:36:03 +0100

    Setup(core)

---
+++ core/types.h
@@ -0,0 +1,5 @@
+#pragma once
+
+// enum Type keeps record of all kinds of base types.
+// The real issue is how to actually define those in C code...
+enum Type { TypeByte, TypeInt, TypeFloat, TypePair, TypeFunc };
