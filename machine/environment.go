2ca9298ac6b3fcd4afafcbe74b54571d8b7c9d90
Viktor A. Rozenko Voitenko <sharp.vik@gmail.com>
2022-08-13 11:51:51 +0100

    style(machine): extract start into a separate file

--- machine/environment.go
+++ machine/environment.go
@@ -1,10 +1,5 @@
 package machine
 
-import (
-	"log"
-	"os"
-)
-
 type Env struct {
 	Data Data
 	Code Code
@@ -20,25 +15,5 @@ func NewEnv(
 		Data: data,
 		Code: code,
 		Core: core,
-	}
-}
-
-func Start(env *Env) {
-	log.Print("starting runtime from environment ...")
-	defer gracefulExit()
-	setupAndStart(env)
-}
-
-func setupAndStart(env *Env) {
-	log.Print("initialising the main thunk ...")
-	main := MainTask(env)
-	log.Print("starting execution by head call ...")
-	main.Call()
-}
-
-func gracefulExit() {
-	if err := recover(); err != nil {
-		log.Println("hasky exited with error:", err)
-		os.Exit(1)
 	}
 }
